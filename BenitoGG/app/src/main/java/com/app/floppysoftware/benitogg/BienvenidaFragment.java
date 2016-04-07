9ef62e1c5a5df97f29a4ff56be27d9248050559f
Miguel Garcia <miguelvisgarcia@gmail.com>
2016-04-07 23:55:44 +0200

    app: revisados comentarios en fragment de bienvenida

--- BenitoGG/app/src/main/java/com/app/floppysoftware/benitogg/BienvenidaFragment.java
+++ BenitoGG/app/src/main/java/com/app/floppysoftware/benitogg/BienvenidaFragment.java
@@ -1,30 +1,38 @@
 package com.app.floppysoftware.benitogg;
 
-
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-
 /**
- * A simple {@link Fragment} subclass.
+ * Fragment de bienvenida. Su único propósito es servir de fragment
+ * inicial cuando el dispositivo es una tablet.
  */
 public class BienvenidaFragment extends Fragment {
 
-
+    /**
+     * Constructor.
+     */
     public BienvenidaFragment() {
-        // Required empty public constructor
+
+        // Nada de momento
     }
 
 
+    /**
+     * Método para inflar el layout del fragment.
+     *
+     * @param inflater   Inflater
+     * @param container  Contenedor
+     * @param savedInstanceState  Estado previamente guardado
+     * @return  Layout inflado
+     */
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        // Inflar el layout del fragment
         return inflater.inflate(R.layout.fragment_bienvenida, container, false);
     }
-
-
 }
