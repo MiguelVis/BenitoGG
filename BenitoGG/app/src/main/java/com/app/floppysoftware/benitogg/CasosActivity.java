e3839dabea1201d6f61c305e235e5793d42dcb84
Miguel Garcia <miguelvisgarcia@gmail.com>
2016-04-16 19:02:40 +0200

    app: optimización y comentarios de la activity de casos

--- BenitoGG/app/src/main/java/com/app/floppysoftware/benitogg/CasosActivity.java
+++ BenitoGG/app/src/main/java/com/app/floppysoftware/benitogg/CasosActivity.java
@@ -16,8 +16,12 @@ import android.widget.TextView;
 import java.util.ArrayList;
 
 /**
- * Activity para mostrar la lista de nombres de casos, así como el
- * estado en que se encuentran, y un texto informativo.
+ * Activity para mostrar la lista de casos, así como el estado en el que se
+ * encuentran (resuelto / no resuelto), y un texto informativo.
+ *
+ * @author   Miguel I. García López
+ * @version  2.0
+ * @since    16 Apr 2016
  */
 public class CasosActivity extends Activity {
 
@@ -36,10 +40,7 @@ public class CasosActivity extends Activity {
         // Llamar a la superclase
         super.onCreate(savedInstanceState);
 
-        //
-        super.onCreate(savedInstanceState);
-
-        // Averiguar si el dispositivo es una tablet con orientación horizontal
+        // Averiguar si el dispositivo es una tablet con preferencia de orientación horizontal
         boolean esTabletHorizontal = getResources().getBoolean(R.bool.isTablet) && !Preferencias.getVertical(this);
 
         // Cambiar la orientación de la pantalla. Esto puede ocasionar que la
@@ -49,7 +50,6 @@ public class CasosActivity extends Activity {
                 setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
             }
         } else {
-
             if(getRequestedOrientation() != ActivityInfo.SCREEN_ORIENTATION_PORTRAIT) {
                 setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
             }
@@ -59,10 +59,10 @@ public class CasosActivity extends Activity {
         setContentView(R.layout.activity_casos);
 
         // Tomar la referencia de los views
-        listViewCasos = (ListView) findViewById(R.id.listViewCasos);
-        textViewCasos = (TextView) findViewById(R.id.textViewCasos);
+        listViewCasos = (ListView) findViewById(R.id.listViewCasos);  // Lista de casos
+        textViewCasos = (TextView) findViewById(R.id.textViewCasos);  // Texto adicional
 
-        // Mostrar lista de casos resueltos
+        // Mostrar lista de casos
         new MostrarCasosResueltos().execute();
     }
 
@@ -72,18 +72,16 @@ public class CasosActivity extends Activity {
      */
     private class MostrarCasosResueltos extends AsyncTask<Void, Void, ArrayList<Caso>> {
 
-        // Número total de casos
-        private int numTotalCasos;
-
         /**
          * Tarea a realizar en background, fuera del thread del UI. Devuelve
          * la lista de casos.
          *
-         * @param params  nada
-         * @return  casos
+         * @param params  Nada
+         * @return        Lista de casos
          */
         @Override
         protected ArrayList<Caso> doInBackground(Void... params) {
+
             // Abrir base de datos
             BaseDatos bd = new BaseDatos(CasosActivity.this, false);
 
@@ -93,14 +91,14 @@ public class CasosActivity extends Activity {
             // Cerrar base de datos
             bd.cerrar();
 
-            // Ofuscar el nombre de los casos no resueltos
+            // Alterar el nombre de los casos no resueltos
             for(Caso caso: casos) {
                 if(!caso.getResuelto()) {
-                    caso.setNombre(ofuscarTexto(caso.getNombre()));
+                    caso.setNombre(alterarTexto(caso.getNombre()));
                 }
             }
 
-            // Devolver lista
+            // Devolver lista de casos
             return casos;
         }
 
@@ -114,17 +112,24 @@ public class CasosActivity extends Activity {
         protected void onPostExecute(ArrayList<Caso> casos) {
 
             // Crear adapter con la lista de casos para el ListView
-            //ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, listaCasosResueltos);
             CasosAdapter adapter = new CasosAdapter(casos);
 
             // Asignar el adapter al ListView
             listViewCasos.setAdapter(adapter);
 
-            // Texto informativo sobre los casos resueltos.
+            // Texto informativo sobre los casos resueltos (por
+            // defecto: todos resueltos).
             int textoId = R.string.casos_resueltos_todos;
 
+            // Cambiar el texto informativo si hay casos pendientes
+            // de resolver
             for(Caso caso : casos) {
+
+                // Si hay al menos un caso no resuelto,
+                // cambiar el texto.
                 if(!caso.getResuelto()) {
+
+                    // Cambiar el texto; quedan casos por resolver
                     textoId = R.string.casos_resueltos_quedan;
                     break;
                 }
@@ -136,13 +141,13 @@ public class CasosActivity extends Activity {
     }
 
     /**
-     * Ofuscar un texto, sustituyendo los caracteres en posiciones impares,
+     * Alterar un texto, sustituyendo los caracteres en posiciones impares,
      * que no sean espacios, por el caracter '?'.
      *
-     * @param texto   texto a ofuscar
-     * @return        texto resultante
+     * @param texto   Texto a alterar
+     * @return        Texto resultante
      */
-    private String ofuscarTexto(String texto) {
+    private String alterarTexto(String texto) {
 
         // Texto resultante
         String resultado = "";
@@ -150,7 +155,7 @@ public class CasosActivity extends Activity {
         // Recorrer el texto
         for(int i = 0; i < texto.length(); ++i) {
 
-            // Caracter en la posición actual
+            // Tomar caracter que está en la posición actual
             char ch = texto.charAt(i);
 
             // Si la posición es impar, y el caracter no
@@ -167,34 +172,53 @@ public class CasosActivity extends Activity {
         return resultado;
     }
 
+    /**
+     * Adapter para el ListView.
+     */
     private class CasosAdapter extends ArrayAdapter<Caso> {
 
+        // Datos de la lista
         private ArrayList<Caso> datos;
 
+        /**
+         * Constructor.
+         *
+         * @param datos  Datos de la lista
+         */
         public CasosAdapter(ArrayList<Caso> datos) {
 
+            // Llamar a la superclase
             super(CasosActivity.this, 0, datos);
 
+            // Tomar lista de datos
             this.datos = datos;
         }
 
-        //
+        /**
+         * Devolver el view correspondiente a un caso de la lista.
+         *
+         * @param position     Posición del caso en la lista
+         * @param convertView  View a tratar
+         * @param parent       View padre
+         * @return             View tratado
+         */
         public View getView(int position, View convertView, ViewGroup parent) {
-            // position del view
-            // converView -> view a utilizar
-            // parent -> padre
 
-            LayoutInflater inflater = (LayoutInflater) CasosActivity.this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-
+            // Inflar el layout del view si no está inflado ya
             if(convertView == null) {
+
+                // Inflater
+                LayoutInflater inflater = (LayoutInflater) CasosActivity.this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+                // Inflar el layout del view
                 convertView = inflater.inflate(R.layout.adapter_casos, parent, false);
             }
 
             // Tomar los views del layout
-            ImageView imageViewTick = (ImageView) convertView.findViewById(R.id.imageViewAdapterCasos);
-            TextView textViewCaso = (TextView) convertView.findViewById(R.id.textViewAdapterCasos);
+            ImageView imageViewTick = (ImageView) convertView.findViewById(R.id.imageViewAdapterCasos);  // Tick de estado
+            TextView  textViewCaso  = (TextView) convertView.findViewById(R.id.textViewAdapterCasos);    // Nombre del caso
 
-            // Tomar el caso que corresponde
+            // Tomar el caso correspondiente
             Caso caso = datos.get(position);
 
             // Fijar imagen del tick según el estado del caso
@@ -205,7 +229,6 @@ public class CasosActivity extends Activity {
 
             // Devolver el view
             return convertView;
-
         }
     }
 }
